72aad825e2e5a982ea988c58c9d53ef70f1a07d3
Andrei Smirnov <andrei.smirnov.69@gmail.com>
2022-12-21 10:56:30 -0500

    start initial python module setup

---
+++ setup.py
@@ -0,0 +1,8 @@
+# using https://stackoverflow.com/a/55266233/1181482 as starting point
+
+from setuptools import setup, find_packages
+
+setup(name="pyjviz",
+      version="0.0",
+      packages=['pyjviz']
+      )
