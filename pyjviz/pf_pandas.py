99d0147e5ae0546080d2b8f0ef9e214603d4a173
Andrei Smirnov <andrei.smirnov.69@gmail.com>
2023-01-09 02:32:47 -0500

    assign kind of work

--- pyjviz/pf_pandas.py
+++ pyjviz/pf_pandas.py
@@ -9,13 +9,15 @@ from . import obj_tracking
 from . import methods_chain
 
 class CallbackObj:
-    def __init__(self, func):
+    def __init__(self, chain_path, func):
+        self.chain_path = chain_path
         self.func = func
         self.ret = None
         self.uri = None
-        
+
     def __call__(self, *args, **kwargs):
         self.ret = self.func(*args, **kwargs)
+        #ipdb.set_trace()
         return self.ret
 
 class DataFrameAttr:
@@ -23,18 +25,27 @@ class DataFrameAttr:
         self.func = func
 
     def __call__(self, *x, **y):
-        print("DataFrameAttr __call__", x, y)
-        #ipdb.set_trace()
-        ret = self.func(*x, **y)
-
+        print("DataFrameAttr __call__", x[1], y)
         rdfl = rdflogging.rdflogger
-        x0_t_obj = obj_tracking.tracking_store.get_tracking_obj(x[0])
-        x0_uri = rdfl.register_obj(x[0], x0_t_obj)
-        t_ret = obj_tracking.tracking_store.get_tracking_obj(ret)
-        ret_uri = rdfl.register_obj(ret, t_ret)
-        rdfl.dump_triple__(ret_uri, "<df-projection>", x0_uri)
+        if methods_chain.curr_methods_chain is None:
+            ret_obj = self.func(*x, **y)
+        else:
+            chain_path = methods_chain.curr_methods_chain.get_path()
         
-        return ret
+            x0_obj = x[0]
+            ret_obj = self.func(*x, **y)
+
+            x0_t_obj = obj_tracking.tracking_store.get_tracking_obj(x0_obj)
+            if x0_t_obj.last_obj_state_uri is None:
+                x0_t_obj.last_obj_state_uri = rdfl.dump_obj_state(chain_path, x0_obj, x0_t_obj)
+
+            ret_t_obj = obj_tracking.tracking_store.get_tracking_obj(ret_obj)
+            if ret_t_obj.last_obj_state_uri is None:
+                ret_t_obj.last_obj_state_uri = rdfl.dump_obj_state(chain_path, ret_obj, ret_t_obj)
+
+            rdfl.dump_triple__(ret_t_obj.last_obj_state_uri, "<df-projection>", x0_t_obj.last_obj_state_uri)
+        
+        return ret_obj
 
 class Caller_to_datetime:
     def __init__(self, func):
@@ -58,6 +69,15 @@ def enable_pf_pandas__():
     print("pf_pandas.py: register start_method_call")
     pf.register.start_method_call = start_method_call
 
+    old_DataFrame_init = pd.DataFrame.__init__
+    def aux_init(func, *x, **y):
+        #print("aux init")
+        #ipdb.set_trace()
+        ret = func(*x, **y)
+        return ret
+    
+    pd.DataFrame.__init__ = lambda *x, **y: aux_init(old_DataFrame_init, *x, **y)
+    
     if 1:
         old_getattr = pd.DataFrame.__getattr__
         pd.DataFrame.__getattr__ = lambda *x, **y: DataFrameAttr(old_getattr)(*x, *y)
@@ -78,7 +98,8 @@ def enable_pf_pandas__():
     old_drop = pd.DataFrame.drop; del pd.DataFrame.drop
     old_rename = pd.DataFrame.rename; del pd.DataFrame.rename
     old_assign = pd.DataFrame.assign; del pd.DataFrame.assign
-
+    old_copy = pd.DataFrame.copy
+    
     old_combine_first = pd.Series.combine_first
     
     @pf.register_dataframe_method
@@ -97,30 +118,23 @@ def enable_pf_pandas__():
 
     @pf.register_dataframe_method
     def rename(df: pd.DataFrame, columns) -> pd.DataFrame:
+        print("my rename", id(df))
+        #ipdb.set_trace()
         ret = old_rename(df, columns = columns)
-        #print("my rename", id(df), id(ret))
         return ret
 
     @pf.register_dataframe_method
     def assign(df: pd.DataFrame, **kw) -> pd.DataFrame:
-        if 1:
-            #ret = old_assign(df, **kw).copy(False)
-            ret = old_assign(df, **kw)
-        else:
-            print("df:", id(df))
-            ret = df.copy()
-            print("ret:", id(ret))
-            
-            for col, v in kw.items():
-                orig_ret_id = id(ret)
-                ret[col] = v(ret) if callable(v) else v
-                print("ret ids:", orig_ret_id, id(ret))
-                
-            ret = ret.copy(False)
-            
+        ret = old_assign(df, **kw)
         #print("my assign", id(df), id(ret))
         return ret
 
+    @pf.register_dataframe_method
+    def copy(df: pd.DataFrame, *x, **y) -> pd.DataFrame:
+        print("new copy:", x, y)
+        ret = old_copy(df, *x, **y)
+        return ret
+    
     @pf.register_series_method
     def combine_first(s: pd.Series, *args, **kw) -> pd.Series:
         ret = old_combine_first(s, *args, **kw)
@@ -144,7 +158,7 @@ class MethodCallHandler:
         #ipdb.set_trace()
         for k, arg in self.method_kwargs.items():
             if inspect.isfunction(arg):
-                self.method_kwargs[k] = CallbackObj(arg) # create empty callback obj as placeholder for future results
+                self.method_kwargs[k] = CallbackObj(methods_chain.curr_methods_chain.get_path(), arg) # create empty callback obj as placeholder for future results
             else:
                 self.method_kwargs[k] = arg
 
@@ -171,7 +185,6 @@ class MethodCallHandler:
                 if arg_t_obj.last_obj_state_uri is None:
                     arg_t_obj.last_obj_state_uri = rdfl.dump_obj_state(self.chain_path, arg_obj.ret, arg_t_obj)
                 rdfl.dump_triple__(arg_obj.uri, "<ret-val>", arg_t_obj.last_obj_state_uri)
-
    
 def start_method_call(obj, method_name, method_args, method_kwargs):
     print("start_method_call", id(obj))
