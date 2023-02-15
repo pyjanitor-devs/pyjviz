b128a1ee2a307d2a89386365295509e4aaaf1c30
Eric Ma <ericmajinglong@gmail.com>
2023-02-15 11:19:20 -0500

    Code formatting.

--- pyjviz/__init__.py
+++ pyjviz/__init__.py
@@ -1,23 +1,29 @@
 from .fstriplestore import set_triple_store__, FSTripleOutputOneShot
-from .wb_stack import get_wb_stack
-from .wb_stack_entries import *
-from .obj_utils import *
-from .viz import *
-from .rdf_node import arrow
+import os
+import sys
 
 from .pf_pandas import enable_pf_pandas__
 
-pyjviz_enabled = not ('PYJVIZ_DISABLED' in os.environ and os.environ.get('PYJVIZ_DISABLED').lower() in ['1', 'true','yes'])
+pyjviz_enabled = not (
+    "PYJVIZ_DISABLED" in os.environ
+    and os.environ.get("PYJVIZ_DISABLED").lower() in ["1", "true", "yes"]
+)
 
 if pyjviz_enabled:
-    if os.path.basename(sys.argv[0]) == 'ipykernel_launcher.py':
-        tstr = FSTripleOutputOneShot(None, None) # -- NB: how can we detect that this is notebook run
-    else:        
-        pyjviz_output_dir = os.environ.get('PYJVIZ_OUTPUT_DIR') if 'PYJVIZ_OUTPUT_DIR' in os.environ else "~/.pyjviz"            
+    if os.path.basename(sys.argv[0]) == "ipykernel_launcher.py":
+        tstr = FSTripleOutputOneShot(
+            None, None
+        )  # -- NB: how can we detect that this is notebook run
+    else:
+        pyjviz_output_dir = (
+            os.environ.get("PYJVIZ_OUTPUT_DIR")
+            if "PYJVIZ_OUTPUT_DIR" in os.environ
+            else "~/.pyjviz"
+        )
         pyjviz_output_dir = os.path.expanduser(pyjviz_output_dir)
-        tstr = FSTripleOutputOneShot(pyjviz_output_dir,
-                                     os.path.basename(sys.argv[0]) + ".ttl")
+        tstr = FSTripleOutputOneShot(
+            pyjviz_output_dir, os.path.basename(sys.argv[0]) + ".ttl"
+        )
 
     set_triple_store__(tstr)
     enable_pf_pandas__()
-
--- pyjviz/wb_stack.py
+++ pyjviz/wb_stack.py
@@ -1,6 +1,7 @@
 from . import fstriplestore
 from . import rdf_node
 
+
 class WithBlock(rdf_node.RDFNode):
     """
     Code blocks in python - https://docs.python.org/3/reference/executionmodel.html#:~:text=A%20Python%20program%20is%20constructed,typed%20interactively%20is%20a%20block.
@@ -8,15 +9,20 @@ class WithBlock(rdf_node.RDFNode):
 
     Base class for CodeBlock, MethodCall and NestedCall.
       - uri - to identify itself in graph as node
-      - rdf_type_uri - to identify node type
-    """
+      - rdf_type_uri- to identify node type
+    """  # noqa: E501
+
     def __init__(self, *, rdf_type, label):
         super().__init__(rdf_type, label)
         rdfl = fstriplestore.triple_store
         global wb_stack
-        parent_uri = wb_stack.stack_entries__[-1].uri if wb_stack.size() > 0 else "rdf:nil"
+        parent_uri = (
+            wb_stack.stack_entries__[-1].uri
+            if wb_stack.size() > 0
+            else "rdf:nil"
+        )
         rdfl.dump_triple(self.uri, "<part-of>", parent_uri)
-        
+
     def __enter__(self):
         global wb_stack
         wb_stack.push(self)
@@ -26,20 +32,23 @@ class WithBlock(rdf_node.RDFNode):
         fstriplestore.triple_store.flush()
         global wb_stack
         wb_stack.pop()
-        
+
+
 class WithBlockStack:
     def __init__(self):
         self.stack_entries__ = []
 
     def to_string(self):
-        return ":".join([f"{x.label}@{x.rdf_type}" for x in self.stack_entries__])
-    
+        return ":".join(
+            [f"{x.label}@{x.rdf_type}" for x in self.stack_entries__]
+        )
+
     def size(self):
         return len(self.stack_entries__)
 
     def push(self, e):
         self.stack_entries__.append(e)
-        
+
     def pop(self):
         return self.stack_entries__.pop()
 
@@ -61,9 +70,11 @@ class WithBlockStack:
     def get_parent_of_current_entry(self):
         ret = self.stack_entries__[-1] if self.size() > 0 else None
         return ret
-        
-    
+
+
 wb_stack = WithBlockStack()
+
+
 def get_wb_stack():
     global wb_stack
     return wb_stack
