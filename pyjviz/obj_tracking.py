b7fcabd65086bef9b5d3c77b87c9e85ecec7a34e
Andrei Smirnov <andrei.smirnov.69@gmail.com>
2023-02-21 03:13:59 -0500

    get_last_obj_state_uri fixed

--- pyjviz/obj_tracking.py
+++ pyjviz/obj_tracking.py
@@ -10,7 +10,7 @@ class TrackingStore:
 
     def get_last_obj_state_uri(self, obj_pyid):
         t_obj = self.tracking_objs.get(obj_pyid)
-        return t_obj.last_obj_state_uri if t_obj and t_obj.is_alive() else None
+        return t_obj.last_obj_state.uri if t_obj and t_obj.is_alive() else None
 
     def find_tracking_obj(self, obj):
         t_obj, obj_found = self.get_tracking_obj(obj, add_missing=False)
